d0eaf17d409e772affd64af5a565923ad9199bf9
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2023-03-07 11:47:34 +0100

    startProductionMode

---
+++ sb-api/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const morgan = require("morgan");
+const PORT = process.env.PORT || 8000;
+
+// meddlewares
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcom to Saba Embrodery",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
+
+module.exports = app;
