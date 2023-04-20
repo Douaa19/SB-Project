6462d4b5fef13355be608e64d50a0ef60c490c89
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2023-04-20 04:43:52 +0000

    LastModofication

--- sb-api/src/models/Orders.js
+++ sb-api/src/models/Orders.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
 const Orders = new mongoose.Schema({
-    
-})
+  client_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  item_id: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+});
