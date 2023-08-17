78cf78e69e75ea3af7ae3d6d2e2d177eb38563fb
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2023-08-17 23:13:54 +0100

    ConnectDatabase

--- server/src/controllers/UsersController.js
+++ server/src/controllers/UsersController.js
@@ -1,9 +1,11 @@
-const User = require("../models");
+const { User } = require("../models");
 
 // hendle register
 const handleRegister = async (req, res) => {
   try {
-    console.log(req.body);
+    const { email, username, password, phoneNum, address } = req.body;
+    const userExists = await User.find({ email });
+    console.log(userExists);
   } catch (error) {
     res.status(500).send(error.message);
   }
