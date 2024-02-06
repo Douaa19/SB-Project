8885cc8aee7bbc19bc6ba5b10c9ec1d07f3749fb
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-02-06 15:04:51 +0100

    NoDataCardText

--- client/src/components/organismes/NoDataCard.js
+++ client/src/components/organismes/NoDataCard.js
@@ -4,7 +4,7 @@ function NoDataCard() {
   return (
     <div className="h-[50vh] w-full flex justify-center items-center mb-6">
       <span className="text-gray font-bold text-18 py-8 px-28 rounded-md shadow-lg">
-        This category is empty
+        No items for now
       </span>
     </div>
   );
