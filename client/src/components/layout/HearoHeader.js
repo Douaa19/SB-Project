7330a6e541b5443de6fa7531781081b54a4562c7
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-03-06 17:18:40 +0100

    SlogonInHeader

--- client/src/components/layout/HearoHeader.js
+++ client/src/components/layout/HearoHeader.js
@@ -4,6 +4,8 @@ import Bg1 from "../../assets/images/cristina-gottardi-CSpjU6hYo_0-unsplash.jpg"
 import Bg2 from "../../assets/images/kace-rodriguez-p3OzJuT_Dks-unsplash.jpg";
 import Bg3 from "../../assets/images/tobias-rademacher-NuBvAE6VfSM-unsplash.jpg";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function HearoHeader() {
   const images = [Bg1, Bg2, Bg3];
@@ -29,6 +31,7 @@ function HearoHeader() {
             potion: "absolute",
             display: "flex",
             justifyContent: "space-around",
+            alignItems: "center",
             width: "12%",
           }}>
           {dots}
@@ -41,12 +44,13 @@ function HearoHeader() {
         style={{
           width: "3rem",
           height: "0.2rem",
-          color: "blue",
-          border: "2px #DAB88A solid",
+          border: "1px #DAB88A solid",
           borderRadius: "8px",
-          position: "absolute",
-          bottom: "2rem",
-          margin: "0 1rem",
+          position: "relative",
+          top: "0",
+          left: "0",
+          // bottom: "2rem",
+          margin: "0",
           backgroundColor: "#DAB88A",
         }}></div>
     ),
@@ -54,21 +58,31 @@ function HearoHeader() {
 
   return (
     <div className="w-[100%] md:max-h-full ssm:max-h-[50vh] md:mt-3 ssm:mt-0 block">
+      <div className="z-10 h-[100vh] absolute w-full top-0 left-0 flex justify-center items-center">
+        <div className="flex flex-col items-center">
+          <span className="text-center text-white font-normale text-24">
+            Stitching stories, creating memories
+          </span>
+          <span className="text-36 font-bold text-white">Saba Ebroidery</span>
+        </div>
+      </div>
+
       <Slider {...settings}>
         {images.map((image, index) => (
-          <div className="relative">
-            <div className="shadow z-10 h-[100vh] w-full bg-dark opacity-10 fixed top-0 left-0 text-dark">
-              HELLO
-            </div>
+          <>
+            <div className="z-10 h-[100vh] absolute bg-dark opacity-5 w-full top-0 left-0 flex justify-center items-center"></div>
             <img
               key={index}
               src={image}
               alt="slide"
               className="h-[100vh] w-full"
             />
-          </div>
+          </>
         ))}
       </Slider>
+      <div className="">
+        
+      </div>
     </div>
   );
 }
