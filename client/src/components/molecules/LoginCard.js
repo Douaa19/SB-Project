68c7f81247378a402b84c2094b3a159a3339a396
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-03-22 13:24:49 +0000

    styleLoginCard

--- client/src/components/molecules/LoginCard.js
+++ client/src/components/molecules/LoginCard.js
@@ -9,15 +9,15 @@ function LoginCard() {
 
   return (
     <div className="bg-gray h-screen flex justify-center items-center">
-      <div className="w-[60rem] max-h-[30rem] flex justify-between items-center bg-white rounded-md shadow-md px-4 py-5">
+      <div className="w-[60rem] flex justify-between items-center bg-white rounded-md shadow-md px-4 py-5">
         <div className="background-img h-full w-full">
           <LoginImg />
         </div>
-        <div className="w-full h-[30rem] flex flex-col justify-around p-8">
-          <div className="">
+        <div className="w-full flex flex-col justify-around p-8">
+          <div className="pb-6">
             <h4 className="font-bold text-32 text-center py-2">Login</h4>
           </div>
-          <div className="capitalize flex justify-around w-full items-center text-16 font-semibold">
+          <div className="capitalize flex justify-around w-full items-center text-16 font-semibold pb-10">
             <div
               className={`w-full mx-1 flex flex-col justify-center items-center`}>
               <span className="hover:cursor-pointer pb-2">Sign in</span>
