b41e87ec9b1c8b0105c76e78bb6a99bdd7a57e43
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-03-31 16:12:48 +0000

    loginCard

--- client/src/components/molecules/LoginCard.js
+++ client/src/components/molecules/LoginCard.js
@@ -1,11 +1,93 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { Form } from "../templates";
 import { Input, Button } from "../atoms";
+import { ReactComponent as OpenEye } from "../../assets/icons/eye-open-svgrepo-com (1).svg";
+import { ReactComponent as CloseEye } from "../../assets/icons/eye-closed-svgrepo-com.svg";
 import { ReactComponent as LoginImg } from "../../assets/images/Mobile-login-pana.svg";
+import {
+  loginAction,
+  setIdAction,
+  setRoleAction,
+} from "../../redux/actions/auth";
+import { login } from "../../services/auth";
+import { jwtDecode } from "jwt-decode";
 
 function LoginCard(props) {
+  const dispatch = useDispatch();
   const [signIn, setSignIn] = useState("login");
   const [resetInputs, setResetInputs] = useState(false);
+
+  const [data, setData] = useState({});
+  const [errors, setErrors] = useState({});
+  const [passwordType, setPasswordType] = useState("password");
+  const [passwordIcon, setPasswordIcon] = useState(<CloseEye />);
+  const [forgetPassword, setForgetPassword] = useState(false);
+  const [errorResponse, setErrorResponse] = useState("");
+  const forgetPasswordPopup = () => {
+    setForgetPassword(true);
+  };
+
+  const togglePassword = () => {
+    if (passwordType === "password") {
+      setPasswordType("text");
+      setPasswordIcon(<OpenEye />);
+      return;
+    }
+    setPasswordType("password");
+    setPasswordIcon(<CloseEye />);
+  };
+  const handleChange = async (element, value) => {
+    const newData = { ...data, [`${element}`]: value };
+    setData(newData);
+  };
+  const handleSubmit = async () => {
+    let errors = validationForm(data);
+
+    if (Object.keys(errors).length === 0) {
+      login(data).then(async (response) => {
+        if (!response.data.token) {
+          alert("Credentials are invalid");
+          setErrorResponse("Credentials are invalid");
+          return errors;
+        } else {
+          await dispatch(loginAction());
+          await dispatch(setRoleAction(jwtDecode(response.data.token).role));
+          await dispatch(setIdAction(jwtDecode(response.data.token).id));
+          setTimeout(() => {
+            window.location = "/";
+          });
+        }
+      });
+    } else {
+      console.log("Error!!");
+      console.log(errors);
+    }
+  };
+  const validationForm = (data) => {
+    const errors = {};
+
+    if (!data.email) {
+      errors.email = "Email is required";
+    } else if (!isValidEmail(data.email)) {
+      errors.email = "Invalid email address";
+    }
+
+    if (props.type === "login") {
+      if (!data.password) {
+        errors.password = "password is required";
+      }
+    }
+
+    setErrors(errors);
+    return errors;
+  };
+
+  const isValidEmail = (email) => {
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  };
 
   useEffect(() => {
     setResetInputs(true);
@@ -45,19 +127,10 @@ function LoginCard(props) {
       </span>
       <div className="flex items-center justify-start w-full">
         <Button
-          className={`
-          ${props.buttonClass ? `${props.buttonClass} ssm:m-0 md:mt-3` : ""} 
+          className={`w-full mt-4 ssm:m-0 md:mt-3
           border-1 border-main rounded-md md:px-10 ssm:px-6 md:py-3 ssm:py-[6px] capitalize text-white md:text-16 ssm:text-12 outline-none hover:bg-white hover:text-main bg-main font-bold`}
-          text={
-            props.type === "contact"
-              ? "submit"
-              : props.type === "login"
-              ? "login"
-              : props.type === "createAccount"
-              ? "create account"
-              : "checkout"
-          }
-          // onClick={() => handleSubmit()}
+          text="sign in"
+          onClick={() => handleSubmit()}
         />
       </div>
       {/* <Form
