99e4892e5f94c639125aba7d35b1b717ed710f83
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-04-16 00:12:34 +0100

    sendDataToBack

--- client/src/services/orders.js
+++ client/src/services/orders.js
@@ -2,11 +2,15 @@ import axios from "axios";
 const BACK_URL = "http://localhost:8008/api/orders";
 const token = localStorage.getItem("token");
 
-
-export const sendOrder = (data) => {
-  const res = axios.post(`${BACK_URL}/create-order`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const sendOrder = (shipping, items) => {
+  console.log(shipping, items);
+  const res = axios.post(
+    `${BACK_URL}/create-order`,
+    { shipping, items },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 };
