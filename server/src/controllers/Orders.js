eb66cff8d35a6dd71e38db6c5766759dc2468e91
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-04-16 13:56:21 +0100

    removeFindItems

--- server/src/controllers/Orders.js
+++ server/src/controllers/Orders.js
@@ -1,28 +1,39 @@
-const { Order } = require("../models");
+const { Order, Item } = require("../models");
 
 const createOrder = async (req, res) => {
-  console.log(req.body, req.user);
-  // try {
-  // const client_id = req.user.id;
-  // const { item_id } = req.body;
-  // let newOrder = await Order.create({ client_id, item_id });
-  // newOrder = await newOrder.populate(
-  //   "client_id",
-  //   "username email address phoneNum"
-  // );
-  // newOrder = await newOrder.populate(
-  //   "item_id",
-  //   "title description color images size price category_id"
-  // );
-  // send email to the admin includes newOrder variable
-  // send result
-  //   res.status(200).send(newOrder);
-  // } catch (error) {
-  //   res.status(500).send({
-  //     messageError: "Somthing goes wrong in server side",
-  //     error: error.message,
-  //   });
-  // }
+  try {
+    const client_id = req.user.id;
+    const shipping = 10;
+    let totalPrices = [];
+    let order = [];
+
+    req.body.items.forEach((item) => {
+      const itemId = item.item._id;
+      // calculate the price (quantity * item.price)
+      const priceItemQuantity = product.price * item.quantity;
+      // push prices into totalPrices
+      totalPrices.push(priceItemQuantity);
+    });
+
+    // const { item_id } = req.body;
+    // let newOrder = await Order.create({ client_id, item_id });
+    // newOrder = await newOrder.populate(
+    //   "client_id",
+    //   "username email address phoneNum"
+    // );
+    // newOrder = await newOrder.populate(
+    //   "item_id",
+    //   "title description color images size price category_id"
+    // );
+    // send email to the admin includes newOrder variable
+    // send result
+    //   res.status(200).send(newOrder);
+  } catch (error) {
+    res.status(500).send({
+      messageError: "Somthing goes wrong in server side",
+      error: error.message,
+    });
+  }
 };
 
 // get my orders
