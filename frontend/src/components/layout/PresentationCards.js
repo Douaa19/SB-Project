832ac1d25eab1137b31bf9a0fff6a06b63890c3d
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-10-11 13:13:50 +0100

    animationHoverPresentationCards

--- frontend/src/components/layout/PresentationCards.js
+++ frontend/src/components/layout/PresentationCards.js
@@ -1,18 +1,20 @@
-import React from "react";
-import Icon from "../../assets/icons/box-svgrepo-com.svg";
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import Box from "../../assets/icons/cube.png";
 import Embroidery from "../../assets/icons/craft-embroidery-crafts-svgrepo-com.svg";
+import EcoFriendly from "../../assets/icons/ecofriendly.png";
 
 import PresentationCard from "../organismes/PresentationCard";
 
 function PresentationCards() {
   const items = [
     {
-      icon: Icon,
+      icon: Box,
       title: "Production process",
       desc: "Skilled artisans meticulously craft each piece from start to finish, using high-quality fabrics and threads. Attention to detail is our priority, ensuring exquisite designs with every stitch.",
     },
     {
-      icon: Icon,
+      icon: EcoFriendly,
       title: "Ico Friendly",
       desc: "Saba Embroidery is committed to sustainability. We use organic materials and eco-friendly practices, supporting a greener future with every embroidered creation.",
     },
@@ -22,11 +24,37 @@ function PresentationCards() {
       desc: "To preserve the beauty of your embroidered treasures from Saba Embroidery, hand wash gently in cold water, air dry flat, and store away from direct sunlight.",
     },
   ];
+
+  let [hoveredIndex, setHoveredIndex] = useState(null);
+
   return (
-    <div className="w-full ssm:py-6 sm:py-14 px-10 flex justify-center items-center">
-      <div className="w-full max-h-max h-max flex flex-wrap justify-center items-center gap-6 transition-all duration-300 ease">
-        {items.map((item, index) => (
-          <PresentationCard item={item} key={index} />
+    <div className="max-w-6xl mx-auto px-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10">
+        {items.map((item, idx) => (
+          <div
+            key={item?.link}
+            className="relative group block p-2 h-full w-full "
+            onMouseEnter={() => setHoveredIndex(idx)}
+            onMouseLeave={() => setHoveredIndex(null)}>
+            <AnimatePresence>
+              {hoveredIndex === idx && (
+                <motion.span
+                  className="absolute inset-0 h-full w-full bg-slate-800/[0.1] block rounded-lg"
+                  layoutId="hoverBackground"
+                  initial={{ opacity: 0 }}
+                  animate={{
+                    opacity: 1,
+                    transition: { duration: 0.15 },
+                  }}
+                  exit={{
+                    opacity: 0,
+                    transition: { duration: 0.15, delay: 0.2 },
+                  }}
+                />
+              )}
+            </AnimatePresence>
+            <PresentationCard item={item} />
+          </div>
         ))}
       </div>
     </div>
--- frontend/src/components/organismes/PresentationCard.js
+++ frontend/src/components/organismes/PresentationCard.js
@@ -2,21 +2,36 @@ import React from "react";
 
 function PresentationCard({ item }) {
   return (
-    <div className="ssm:p-4 sm:p-6 w-400 max-w-500 min-w-300 gap-6 border rounded-lg border-main flex justify-between items-center">
-      <div className="sm:w-20 sm:max-w-20 sm:min-w-20 ssm:max-w-16 ssm:min-w-16">
-        <img
-          src={item.icon}
-          alt="img"
-          className="w-16 max-w-16 min-w-16 ssm:w-12 ssm:maw-w-12"
-        />
-      </div>
-      <div className="flex flex-col gap-1">
-        <span className="font-bold sm:text-16 capitalize sm:pb-2 ssm:text-12 ssm:pb-1">
-          {item.title}
-        </span>
-        <p className="sm:text-14 ssm:text-10">{item.desc}</p>
+    <div className="rounded-lg h-full w-full p-4 overflow-hidden bg-gradient-to-br from-slate-800 to-slate-800/[0.2] border border-gray-100 group-hover:border-slate-700 relative z-50">
+      <div className="relative z-50 p-4">
+        <div className="md:w-10 ssm:w-8">
+          <img src={item.icon} alt={item.title} className="w-full" />
+        </div>
+        <div className="">
+          <h4 className="text-zinc-100 font-bold tracking-wide mt-2">
+            {item.title}
+          </h4>
+          <p className="mt-4 text-zinc-400 tracking-wide leading-relaxed text-sm">
+            {item.desc}
+          </p>
+        </div>
       </div>
     </div>
+    // <div className="md:text-16 ssm:text-14 ssm:p-4 sm:p-6 border rounded-lg border-main max-w-sm">
+    //   <div className="md:w-10 ssm:w-8 mb-2">
+    //     <img
+    //       src={item.icon}
+    //       alt={item.title}
+    //       className="w-full"
+    //     />
+    //   </div>
+    //   <div className="flex flex-col gap-1">
+    //     <span className="font-bold capitalize mb-1">
+    //       {item.title}
+    //     </span>
+    //     <p className="text-14">{item.desc}</p>
+    //   </div>
+    // </div>
   );
 }
 
