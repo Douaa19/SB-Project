18720e2055a10e1cd7f2cde7b14e605f9ccd1de9
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-10-31 17:22:38 +0100

    OrderSummaryCard

--- frontend/src/components/organismes/OrderSummaryCard.js
+++ frontend/src/components/organismes/OrderSummaryCard.js
@@ -1,13 +1,46 @@
 import React from "react";
 import { BACK_URL } from "../../config";
+import { useDispatch } from "react-redux";
+import { editOrder, removeOrder } from "../../redux/actions/orders";
 
 function OrderSummaryCard(props) {
+  const dispatch = useDispatch();
   const itemImg = `${BACK_URL}/items/${props.order.item._id}/image`;
-  console.log(props);
+
+  const handleQuantityChange = (operation) => {
+    if (operation === "add") {
+      const newQuantity = props.order.quantity + 1;
+      dispatch(
+        editOrder(
+          props.userId,
+          props.order.item._id,
+          props.order.colors,
+          newQuantity
+        )
+      );
+    } else if (operation === "subtract") {
+      if (props.order.quantity === 1) {
+        dispatch(
+          removeOrder(props.userId, props.order.item._id, props.order.colors)
+        );
+      } else if (props.order.quantity > 1) {
+        const newQuantity = props.order.quantity - 1;
+        dispatch(
+          editOrder(
+            props.userId,
+            props.order.item._id,
+            props.order.colors,
+            newQuantity
+          )
+        );
+      }
+    }
+  };
+
   return (
-    <div className="w-full py-2">
-      <div className="">
-        <div className={`w-16 h-16 overflow-hidden rounded-md`}>
+    <div className="w-full my-2 flex justify-between items-center">
+      <div className="flex items-cenetr justify-start gap-4">
+        <div className={`w-20 h-20 overflow-hidden rounded-md`}>
           {!itemImg ? (
             <div className="w-full h-full rounded-md animate-pulse bg-gray-100"></div>
           ) : (
@@ -18,8 +51,31 @@ function OrderSummaryCard(props) {
             />
           )}
         </div>
+        <div className="flex flex-col items-start justify-center">
+          <span className="text-14 font-semibold capitalize text-gray-700">
+            {props.order.item.title}
+          </span>
+          <span className="text-14 font-normal text-gray-500 uppercase">
+            {props.order.item.title}
+          </span>
+          <span className="text-14 font-semibold capitalize text-gray-700">
+            {props.order.item.price * props.order.quantity}DH
+          </span>
+        </div>
       </div>
-      <div className=""></div>
+      <div className="border border-gray-100 p-2 rounded-md">
+        <button
+          onClick={() => handleQuantityChange("subtract")}
+          className="text-16 font-semibold text-gray-500 hover:text-gray-700 px-4">
+          -
+        </button>
+        <span className="text-14 font-bold px-2">{props.order.quantity}</span>
+        <button
+          onClick={() => handleQuantityChange("add")}
+          className="text-16 font-semibold text-gray-500 hover:text-gray-700 px-4">
+          +
+        </button>
+      </div>
     </div>
   );
 }
