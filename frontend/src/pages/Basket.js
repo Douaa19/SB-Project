d27334c99071e76f7346c04a0dde0eb1a816b4e0
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-11-01 13:36:42 +0100

    EmptyRowBasketStyle

--- frontend/src/pages/Basket.js
+++ frontend/src/pages/Basket.js
@@ -3,14 +3,11 @@ import { NavBar, Footer, Loading } from "../components/layout";
 import { BasketTable } from "../components/organismes";
 import { PageTitle } from "../components/atoms";
 import { useSelector } from "react-redux";
-import { Popup } from "../components/organismes";
-import { CheckoutPopupContent } from "../components/molecules";
 import { ShoppingBag, MoveLeft, ArrowRight } from "lucide-react";
 
 function Basket() {
   const orders = useSelector((state) => state.orders.orders);
   const userId = useSelector((state) => state.user_id);
-  const orderSent = useSelector((state) => state.orderSentPopup);
   const [loading, setLoading] = useState(true);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -38,34 +35,31 @@ function Basket() {
             <div className="flex items-center justify-center w-full h-[2px] mb-8">
               <span className="bg-gray-100 h-full w-80 rounded-full"></span>
             </div>
-            <div className="hover:border-b hover:border-main w-fit">
-              <a
-                href="/products"
-                className="md:text-16 ssm:text-14 text-gray-700 hover:text-main flex gap-0 items-end justify-start">
-                <MoveLeft size={20} strokeWidth={1.5} /> Back to shopping
-              </a>
-            </div>
+            {userOrders.length > 0 && (
+              <div className="hover:border-b hover:border-main w-fit">
+                <a
+                  href="/products"
+                  className="md:text-16 ssm:text-14 text-gray-700 hover:text-main flex gap-0 items-end justify-start">
+                  <MoveLeft size={20} strokeWidth={1.5} /> Back to shopping
+                </a>
+              </div>
+            )}
             <BasketTable orders={userOrders} />
-            <div className="mt-6">
-              <a
-                href="/checkout"
-                class="relative inline-flex items-center px-8 py-3 overflow-hidden md:text-16 ssm:text-14 font-medium text-main border-1 border-main rounded-md hover:text-white group hover:bg-gray-50 ssm:w-52 sm:w-auto">
-                <span class="absolute left-0 block w-full h-0 transition-all bg-main opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-                <span class="absolute -right-2 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-                  <ArrowRight strokeWidth={1.5} size={24} />
-                </span>
-                <span class="relative w-full text-center">Check out</span>
-              </a>
-            </div>
+            {userOrders.length > 0 && (
+              <div className="mt-6">
+                <a
+                  href={userId.length > 0 ? "/checkout" : "/login"}
+                  class="relative inline-flex items-center px-8 py-3 overflow-hidden md:text-16 ssm:text-14 font-medium text-main border-1 border-main rounded-md hover:text-white group hover:bg-gray-50 ssm:w-52 sm:w-auto">
+                  <span class="absolute left-0 block w-full h-0 transition-all bg-main opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+                  <span class="absolute -right-2 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+                    <ArrowRight strokeWidth={1.5} size={24} />
+                  </span>
+                  <span class="relative w-full text-center">Check out</span>
+                </a>
+              </div>
+            )}
           </div>
           <Footer />
-          {orderSent !== false && (
-            <div className="min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover">
-              <Popup>
-                <CheckoutPopupContent />
-              </Popup>
-            </div>
-          )}
         </>
       )}
     </>
--- frontend/src/pages/Checkout.js
+++ frontend/src/pages/Checkout.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { NavBar, Footer } from "../components/layout";
-import { ShippingForm } from "../components/molecules";
+import { NavBar, Footer, Loading } from "../components/layout";
+import { ShippingForm, CheckoutPopupContent } from "../components/molecules";
 import { Input, Button } from "../components/atoms";
 import { OrderSummaryCard } from "../components/organismes";
 import { sendOrder } from "../services/orders";
 import { setOrderSent } from "../redux/actions/popups";
 import { CircleCheck } from "lucide-react";
+import { Popup } from "../components/organismes";
 
 function Checkout() {
   const dispatch = useDispatch();
@@ -19,6 +20,8 @@ function Checkout() {
   const shipping = 49;
   const userId = useSelector((state) => state.user_id);
   const orders = useSelector((state) => state.orders.orders);
+  const [loading, setLoading] = useState(true);
+  const orderSent = useSelector((state) => state.orderSentPopup);
 
   const userOrders = orders[userId];
 
@@ -91,6 +94,16 @@ function Checkout() {
     return /^(\+212\d{9}|0\d{9})$/.test(phone);
   };
 
+  const SubmitButton = (
+    <Button
+      type="submit"
+      text="Confirm Order"
+      textClass="relative z-10"
+      className="capitalize relative flex px-6 py-3 items-center justify-center overflow-hidden bg-main text-16 font-medium rounded-md text-white shadow-md transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-main hover:shadow-main hover:before:border-[25px]"
+      onClick={handleSubmit}
+    />
+  );
+
   useEffect(() => {
     let total = 0;
     for (let index = 0; index < userOrders.length; index++) {
@@ -110,151 +123,165 @@ function Checkout() {
       const grandTotal = subtotal + shipping;
       setGrandTotal(grandTotal);
     }
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
   }, [subtotal, shipping]);
 
   return (
     <>
-      <NavBar />
-      <div className="ssm:pt-4 md:px-20 ssm:px-16 w-full mt-24">
-        <div className="grid md:grid-cols-2 md:grid-rows-1 ssm:grid-cols-1 ssm:grid-rows-2 gap-8 h-max w-full">
-          <div className="md:col-span-1 ssm:order-last md:order-first w-full flex flex-col items-start justify-start gap-8">
-            <div className="flex flex-col items-start gap-2 w-full">
-              <h3 className="text-16 font-semibold capitalize tracking-wide text-gray-700">
-                delivery information
-              </h3>
-              <ShippingForm
-                className="bg-white p-6 rounded-md shadow-md w-full"
-                button={false}
-                data={data}
-                setData={setData}
-                errors={errors}
-              />
-            </div>
-            <div className="flex flex-col items-start gap-2 w-full">
-              <h3 className="text-16 font-semibold capitalize tracking-wide text-gray-700">
-                payment method
-              </h3>
-              {errors.payment && (
-                <h4 className="text-red font-medium text-14">
-                  {errors.payment}
-                </h4>
-              )}
-              <div className="w-full flex sm:flex-row ssm:flex-col gap-4 items-center justify-between">
-                <div
-                  className={`py-4 px-4 flex w-full items-center bg-white shadow-sm rounded-md ${
-                    selectedPayment === "cash-on-delivery"
-                      ? "border-main border-2"
-                      : "border-gray-100"
-                  }`}
-                  onClick={() => setSelectedPayment("cash-on-delivery")}>
-                  <label className="text-14 font-medium text-gray-700 flex gap-1 items-center">
-                    <Input
-                      type="radio"
-                      value="cash-on-delivery"
-                      checked={selectedPayment === "cash-on-delivery"}
-                      readOnly
-                      className="peer sr-only"
-                    />
-                    <CircleCheck
-                      strokeWidth={1.5}
-                      className={`rounded-full ${
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <NavBar />
+          <div className="ssm:pt-4 md:px-20 ssm:px-16 w-full mt-24">
+            <div className="grid md:grid-cols-2 md:grid-rows-1 ssm:grid-cols-1 ssm:grid-rows-2 gap-8 h-max w-full">
+              <div className="md:col-span-1 ssm:order-last md:order-first w-full flex flex-col items-start justify-start gap-8">
+                <div className="flex flex-col items-start gap-2 w-full">
+                  <h3 className="text-16 font-semibold capitalize tracking-wide text-gray-700">
+                    delivery information
+                  </h3>
+                  <ShippingForm
+                    className="bg-white p-6 rounded-md shadow-md w-full"
+                    button={false}
+                    data={data}
+                    setData={setData}
+                    errors={errors}
+                  />
+                </div>
+                <div className="flex flex-col items-start gap-2 w-full">
+                  <h3 className="text-16 font-semibold capitalize tracking-wide text-gray-700">
+                    payment method
+                  </h3>
+                  {errors.payment && (
+                    <h4 className="text-red font-medium text-14">
+                      {errors.payment}
+                    </h4>
+                  )}
+                  <div className="w-full flex sm:flex-row ssm:flex-col gap-4 items-center justify-between">
+                    <div
+                      className={`py-4 px-4 flex w-full items-center bg-white shadow-sm rounded-md ${
                         selectedPayment === "cash-on-delivery"
-                          ? "bg-main text-light"
-                          : "bg-light text-gray-700"
+                          ? "border-main border-2"
+                          : "border-gray-100"
                       }`}
-                    />
-                    Cash on Delivery
-                  </label>
-                </div>
-                <div
-                  className={`py-4 px-4 flex w-full items-center bg-white shadow-sm rounded-md ${
-                    selectedPayment === "online-payment"
-                      ? "border-main border-2"
-                      : "border-light border-2"
-                  }`}
-                  onClick={() => setSelectedPayment("online-payment")}>
-                  <label className="text-14 font-medium text-gray-700 flex gap-1 items-center">
-                    <Input
-                      type="radio"
-                      value="online-payment"
-                      checked={selectedPayment === "online-payment"}
-                      readOnly
-                      className="appearance-none"
-                    />
-                    <CircleCheck
-                      strokeWidth={1.5}
-                      className={`rounded-full ${
+                      onClick={() => setSelectedPayment("cash-on-delivery")}>
+                      <label className="text-14 font-medium text-gray-700 flex gap-1 items-center">
+                        <Input
+                          type="radio"
+                          value="cash-on-delivery"
+                          checked={selectedPayment === "cash-on-delivery"}
+                          readOnly
+                          className="peer sr-only"
+                        />
+                        <CircleCheck
+                          strokeWidth={1.5}
+                          className={`rounded-full ${
+                            selectedPayment === "cash-on-delivery"
+                              ? "bg-main text-light"
+                              : "bg-light text-gray-700"
+                          }`}
+                        />
+                        Cash on Delivery
+                      </label>
+                    </div>
+                    <div
+                      className={`py-4 px-4 flex w-full items-center bg-white shadow-sm rounded-md ${
                         selectedPayment === "online-payment"
-                          ? "bg-main text-light"
-                          : "bg-light text-gray-700"
+                          ? "border-main border-2"
+                          : "border-light border-2"
                       }`}
-                    />
-                    Online Payment
-                  </label>
+                      onClick={() => setSelectedPayment("online-payment")}>
+                      <label className="text-14 font-medium text-gray-700 flex gap-1 items-center">
+                        <Input
+                          type="radio"
+                          value="online-payment"
+                          checked={selectedPayment === "online-payment"}
+                          readOnly
+                          className="appearance-none"
+                        />
+                        <CircleCheck
+                          strokeWidth={1.5}
+                          className={`rounded-full ${
+                            selectedPayment === "online-payment"
+                              ? "bg-main text-light"
+                              : "bg-light text-gray-700"
+                          }`}
+                        />
+                        Online Payment
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full text-center md:hidden ssm:block">
+                  {SubmitButton}
+                </div>
+              </div>
+              <div className="flex flex-col items-start gap-2 w-full">
+                <h3 className="text-16 font-semibold capitalize tracking-wide text-gray-700">
+                  order summary
+                </h3>
+                <div className="flex flex-col justify-between items-center bg-white p-6 rounded-md shadow-md w-full">
+                  <div className="w-full pb-5">
+                    <div className="flex flex-col items-center justify-center gap-2">
+                      {userOrders.map((order, index) => (
+                        <OrderSummaryCard
+                          key={index}
+                          order={order}
+                          userId={userId}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <div className="flex flex-col items-center justify-center gap-4 border-t-1 border-gray-100 w-full py-5">
+                      <div className="flex justify-between items-center w-full">
+                        <span className="text-14 w-max text-gray-500">
+                          Subtotal
+                        </span>
+                        <span className="text-14 w-max text-gray-700 font-semibold">
+                          {subtotal}DH
+                        </span>
+                      </div>
+                      <div className="flex justify-between items-center w-full">
+                        <span className="text-14 w-max text-gray-500">
+                          Shipping
+                        </span>
+                        <span className="text-14 w-max text-gray-700 font-semibold">
+                          {shipping}DH
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-between w-full border-t-1 border-gray-100 py-5 gap-6">
+                      <div className="flex justify-between items-center w-full">
+                        <span className="text-14 w-max text-gray-500">
+                          Total {`(DH)`}
+                        </span>
+                        <span className="text-14 w-max text-gray-700 font-semibold">
+                          {grandtotal}DH
+                        </span>
+                      </div>
+                      <div className="w-full text-center ssm:hidden md:block">
+                        {SubmitButton}
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-start gap-2 w-full">
-            <h3 className="text-16 font-semibold capitalize tracking-wide text-gray-700">
-              order summary
-            </h3>
-            <div className="flex flex-col justify-between items-center bg-white p-6 rounded-md shadow-md w-full">
-              <div className="w-full pb-5">
-                <div className="flex flex-col items-center justify-center gap-2">
-                  {userOrders.map((order, index) => (
-                    <OrderSummaryCard
-                      key={index}
-                      order={order}
-                      userId={userId}
-                    />
-                  ))}
-                </div>
-              </div>
-              <div className="w-full">
-                <div className="flex flex-col items-center justify-center gap-4 border-t-1 border-gray-100 w-full py-5">
-                  <div className="flex justify-between items-center w-full">
-                    <span className="text-14 w-max text-gray-500">
-                      Subtotal
-                    </span>
-                    <span className="text-14 w-max text-gray-700 font-semibold">
-                      {subtotal}DH
-                    </span>
-                  </div>
-                  <div className="flex justify-between items-center w-full">
-                    <span className="text-14 w-max text-gray-500">
-                      Shipping
-                    </span>
-                    <span className="text-14 w-max text-gray-700 font-semibold">
-                      {shipping}DH
-                    </span>
-                  </div>
-                </div>
-                <div className="flex flex-col items-center justify-between w-full border-t-1 border-gray-100 py-5 gap-6">
-                  <div className="flex justify-between items-center w-full">
-                    <span className="text-14 w-max text-gray-500">
-                      Total {`(DH)`}
-                    </span>
-                    <span className="text-14 w-max text-gray-700 font-semibold">
-                    {grandtotal}DH
-                    </span>
-                  </div>
-                  <div className="w-full text-center">
-                    <Button
-                      type="submit"
-                      text="Confirm Order"
-                      textClass="relative z-10"
-                      className="capitalize relative flex px-6 py-3 items-center justify-center overflow-hidden bg-main md:text-16 ssm:text-14 font-medium rounded-md text-white shadow-md transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-main hover:shadow-main hover:before:border-[25px]"
-                      onClick={handleSubmit}
-                    />
-                  </div>
-                </div>
-              </div>
+          <Footer />
+          {orderSent !== false && (
+            <div className="min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover">
+              <Popup>
+                <CheckoutPopupContent />
+              </Popup>
             </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
+          )}
+        </>
+      )}
     </>
   );
 }
