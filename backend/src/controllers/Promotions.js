542aafae3da8661ea807ae0189a317cc566a5513
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2025-01-30 14:51:10 +0100

    getPromotionsMethod

--- backend/src/controllers/Promotions.js
+++ backend/src/controllers/Promotions.js
@@ -2,12 +2,25 @@ const { Promotion } = require("../models");
 
 //  Get promotions
 const getPromotions = async (req, res) => {
-  console.log("Get Promotions");
+  try {
+    const promotions = await Promotion.find().populate("item_id");
+    if (promotions.length > 0) {
+      res.status(200).send(promotions);
+    } else {
+      res
+        .status(200)
+        .semnd({ message: "Promitions table are empty", promotions });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .send({ messageError: "Somthing goes wrong in bak end", error });
+  }
 };
 
 // Create Promotion
 const createPromotion = async (req, res) => {
-  console.log("Create Promotion");
+  
 };
 
 // Delete Promotion
