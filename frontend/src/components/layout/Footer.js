a4e1bfc6f8c99468a5a2585cd089293f45bf7769
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2025-01-31 13:00:27 +0100

    footerStyleModifications

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -7,18 +7,56 @@ import { motion } from "framer-motion";
 function Footer() {
   return (
     <div
-      className="w-full relative bottom-0 left-0 mt-10 bg-main lg:py-8 ssm:py-6 lg:px-28 ssm:px-16 h-max bg-cover bg-repeat"
+      className="w-full relative bottom-0 left-0 mt-16 bg-main lg:py-8 ssm:py-6 lg:px-28 ssm:px-16 h-max bg-cover bg-repeat"
       style={{ backgroundImage: `url(${FooterBg})` }}>
-      <div className="flex justify-between flex-row items-center">
+      <div className="flex justify-between flex-row items-start mt-4">
         <div className="w-1/2 mb-4 mx-0 flex items-start">
           <a href="/">
-            <img src={Logo} alt="logo" className="md:w-52 ssm:w-36" />
+            <img src={Logo} alt="logo" className="md:w-60 ssm:w-36" />
           </a>
         </div>
         <div className="flex justify-between flex-row w-full pt-4 mb-6 gap-2 ssm:flex-col-reverse">
-          <div className="flex lg:justify-around items-center gap-2 lg:w-full md:w-full ssm:justify-between">
+          <div className="flex lg:justify-between items-start gap-2 lg:w-full md:w-full ssm:justify-between h-auto">
+            <div className="">
+              <h6 className="md:text-xl ssm:text-lg capitalize pb-4 font-bold text-gray-100">
+                main menu
+              </h6>
+              <ul className="text-md text-gray-400">
+                <motion.li
+                  whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className="pb-2 capitalize cursor-pointer">
+                  <a href="/best-selling">best selling</a>
+                </motion.li>
+                <motion.li
+                  whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className="pb-2 capitalize cursor-pointer">
+                  <a href="/products">newest products</a>
+                </motion.li>
+              </ul>
+            </div>
+            <div className="">
+              <h6 className="md:text-xl ssm:text-lg capitalize pb-4 font-bold text-gray-100">
+                helpful informations
+              </h6>
+              <ul className="text-md text-gray-400">
+                <motion.li
+                  whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className="pb-2 capitalize cursor-pointer">
+                  <a href="/about">about us</a>
+                </motion.li>
+                <motion.li
+                  whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className="pb-2 capitalize cursor-pointer">
+                  <a href="/contact">contact us</a>
+                </motion.li>
+              </ul>
+            </div>
             <div className="flex flex-col justify-start items-start md:w-fit ssm:w-1/2 ssm:mt-4 lg:mt-0">
-              <ul className="md:text-14 ssm:text-12 flex flex-col h-max text-gray-400">
+              <ul className="text-md text-gray-400">
                 <li className="pb-2">
                   <div className="flex items-center gap-2">
                     <Phone size={20} className="" strokeWidth={1} />
@@ -33,63 +71,37 @@ function Footer() {
                 </li>
               </ul>
             </div>
-            <div className="">
-              <h6 className="md:text-18 ssm:text-16 capitalize pb-4 font-bold text-gray-100">
-                main menu
-              </h6>
-              <ul className="text-14 text-gray-400">
-                <li className="pb-2 capitalize hover:text-gray-100 hover:cursor-pointer">
-                  <a href="/best-selling">best selling</a>
-                </li>
-                <li className="pb-2 capitalize hover:text-gray-100 hover:cursor-pointer">
-                  <a href="/products">newest products</a>
-                </li>
-              </ul>
-            </div>
-            <div className="">
-              <h6 className="md:text-18 ssm:text-16 capitalize pb-4 font-bold text-gray-100">
-                helpful informations
-              </h6>
-              <ul className="text-14 text-gray-400">
-                <li className="pb-2 capitalize hover:text-gray-100 hover:cursor-pointer">
-                  <a href="/about">about us</a>
-                </li>
-                <li className="pb-2 capitalize hover:text-gray-100 hover:cursor-pointer">
-                  <a href="/contact">contact us</a>
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
       </div>
-      <div className="h-full w-full flex flex-col justify-between">
-        <div className="flex justify-center items-center gap-5 mb-4">
+      <div className="h-full w-full flex flex-col justify-between mt-6">
+        <div className="flex justify-end items-center gap-5">
           <a
             href="https://www.facebook.com/p/Embroidery-Saba-100087891292627/"
             target="_blank"
             rel="noreferrer"
-            className="flex items-center justify-center flex-row-reverse gap-1 border border-gray-400 p-3 rounded-full text-gray-400 hover:text-gray-100">
+            className="flex items-center justify-center flex-row-reverse gap-1 border border-gray-400 p-3 rounded-full text-gray-400 hover:text-white hover:border-white">
             <Facebook strokeWidth={1} size={20} className="" />
           </a>
           <a
             href="https://www.instagram.com/embroiderysaba?igsh=NGVhN2U2NjQ0Yg=="
             target="_blank"
             rel="noreferrer"
-            className="flex items-center justify-center flex-row-reverse gap-1 border border-gray-400 p-3 rounded-full text-gray-400 hover:text-gray-100">
+            className="flex items-center justify-center flex-row-reverse gap-1 border border-gray-400 p-3 rounded-full text-gray-400 hover:text-white hover:border-white">
             <Instagram strokeWidth={1} size={20} className="" />
           </a>
           <a
             href="https://www.pinterest.com/sabaembroidery/"
             target="_blank"
             rel="noreferrer"
-            className="flex items-center justify-center flex-row-reverse gap-1 border border-gray-400 p-3 rounded-full text-gray-400 hover:text-gray-100">
+            className="flex items-center justify-center flex-row-reverse gap-1 border border-gray-400 p-3 rounded-full text-gray-400 hover:text-white hover:border-white">
             <Pin strokeWidth={1} size={20} className="" />
           </a>
         </div>
-        <div className="h-[1px] bg-gray-400 w-full rounded-full mb-2"></div>
-        <div className="text-center w-full">
-          <span className="text-gray-400 md:text-12 ssm:text-10">
-            @ 2024 Saba Embroidery
+        <div className="h-[1px] bg-gray-400 w-full rounded-full mb-4 mt-6"></div>
+        <div className="text-start w-full">
+          <span className="text-gray-400 text-md">
+            Copyright © 2025 Saba Embroidery
           </span>
         </div>
       </div>
