48be8a62c142185bf6d524863db1776a5cb17d3c
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2025-02-07 16:14:23 +0100

    responsiveFooter

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -9,73 +9,71 @@ function Footer() {
     <div
       className="w-full relative bottom-0 left-0 mt-16 bg-main lg:py-8 ssm:py-6 lg:px-28 ssm:px-16 h-max bg-cover bg-repeat"
       style={{ backgroundImage: `url(${FooterBg})` }}>
-      <div className="flex justify-between flex-row items-start mt-4">
-        <div className="w-1/2 mb-4 mx-0 flex items-start">
+      <div className="flex mt-4 md:flex-row ssm:flex-col">
+        <div className="md:w-1/2 ssm:w-full md:mb-4 mx-0 ssm:mb-6">
           <a href="/">
-            <img src={Logo} alt="logo" className="md:w-60 ssm:w-36" />
+            <img src={Logo} alt="logo" className="md:w-60 ssm:w-60" />
           </a>
         </div>
-        <div className="flex justify-between flex-row w-full pt-4 mb-6 gap-2 ssm:flex-col-reverse">
-          <div className="flex lg:justify-between items-start gap-2 lg:w-full md:w-full ssm:justify-between h-auto">
-            <div className="">
-              <h6 className="md:text-xl ssm:text-lg capitalize pb-4 font-bold text-gray-100">
-                main menu
-              </h6>
-              <ul className="text-md text-gray-400">
-                <motion.li
-                  whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="pb-2 capitalize cursor-pointer">
-                  <a href="/best-selling">best selling</a>
-                </motion.li>
-                <motion.li
-                  whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="pb-2 capitalize cursor-pointer">
-                  <a href="/products">newest products</a>
-                </motion.li>
-              </ul>
-            </div>
-            <div className="">
-              <h6 className="md:text-xl ssm:text-lg capitalize pb-4 font-bold text-gray-100">
-                helpful informations
-              </h6>
-              <ul className="text-md text-gray-400">
-                <motion.li
-                  whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="pb-2 capitalize cursor-pointer">
-                  <a href="/about">about us</a>
-                </motion.li>
-                <motion.li
-                  whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="pb-2 capitalize cursor-pointer">
-                  <a href="/contact">contact us</a>
-                </motion.li>
-              </ul>
-            </div>
-            <div className="flex flex-col justify-start items-start md:w-fit ssm:w-1/2 ssm:mt-4 lg:mt-0">
-              <ul className="text-md text-gray-400">
-                <li className="pb-2">
-                  <div className="flex items-center gap-2">
-                    <Phone size={20} className="" strokeWidth={1} />
-                    <span>+212634242755</span>
-                  </div>
-                </li>
-                <li className="pb-2">
-                  <div className="flex items-center gap-2">
-                    <Mail size={20} className="" strokeWidth={1} />
-                    <span>contact@sabaembroidery.com</span>
-                  </div>
-                </li>
-              </ul>
-            </div>
+        <div className=" flex md:justify-between ssm:justify-start items-start md:gap-6 flex-wrap lg:w-full md:w-full h-auto">
+          <div className="">
+            <h6 className="ssm:text-md lg:text-lg capitalize pb-4 font-bold text-gray-100">
+              main menu
+            </h6>
+            <ul className="text-md text-gray-400">
+              <motion.li
+                whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="pb-2 capitalize cursor-pointer">
+                <a href="/best-selling">best selling</a>
+              </motion.li>
+              <motion.li
+                whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="pb-2 capitalize cursor-pointer">
+                <a href="/products">newest products</a>
+              </motion.li>
+            </ul>
+          </div>
+          <div className="ssm:mt-4 md:mt-0">
+            <h6 className="ssm:text-md lg:text-lg capitalize pb-4 font-bold text-gray-100">
+              helpful informations
+            </h6>
+            <ul className="text-md text-gray-400">
+              <motion.li
+                whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="pb-2 capitalize cursor-pointer">
+                <a href="/about">about us</a>
+              </motion.li>
+              <motion.li
+                whileHover={{ x: 8, color: "#f3f4f6" }} // Moves right & changes color
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="pb-2 capitalize cursor-pointer">
+                <a href="/contact">contact us</a>
+              </motion.li>
+            </ul>
+          </div>
+          <div className="flex flex-col justify-start items-start md:w-fit ssm:w-1/2 ssm:mt-4 lg:mt-0">
+            <ul className="md:text-md ssm:text-14 text-gray-400">
+              <li className="pb-2">
+                <div className="flex items-center gap-2">
+                  <Phone size={20} className="" strokeWidth={1} />
+                  <span>+212634242755</span>
+                </div>
+              </li>
+              <li className="pb-2">
+                <div className="flex items-center gap-2">
+                  <Mail size={20} className="" strokeWidth={1} />
+                  <span>contact@sabaembroidery.com</span>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="h-full w-full flex flex-col justify-between mt-6">
-        <div className="flex justify-end items-center gap-5">
+        <div className="flex lg:justify-end items-center gap-5 w-full ssm:justify-center ssm:mt-6">
           <a
             href="https://www.facebook.com/p/Embroidery-Saba-100087891292627/"
             target="_blank"
@@ -99,8 +97,8 @@ function Footer() {
           </a>
         </div>
         <div className="h-[1px] bg-gray-400 w-full rounded-full mb-4 mt-6"></div>
-        <div className="text-start w-full">
-          <span className="text-gray-400 text-md">
+        <div className="flex md:justify-start ssm:justify-center w-full">
+          <span className="text-gray-400 md:text-md ssm:text-14">
             Copyright © 2025 Saba Embroidery
           </span>
         </div>
