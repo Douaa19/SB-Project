ece9a03bc0773541c8c5472eedbb4c88bac04ecd
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2025-02-10 15:23:31 +0100

    ResponsivePresentationsCards

--- frontend/src/components/layout/PresentationCards.js
+++ frontend/src/components/layout/PresentationCards.js
@@ -29,11 +29,11 @@ function PresentationCards() {
 
   return (
     <div className="max-w-6xl mx-auto px-8 mt-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10">
+      <div className="grid grid-cols-1 lg:grid-cols-3 py-10">
         {items.map((item, index) => (
           <div
             key={index}
-            className="relative group block p-2 h-full w-full "
+            className="relative group block p-2 h-full w-full"
             onMouseEnter={() => setHoveredIndex(index)}
             onMouseLeave={() => setHoveredIndex(null)}>
             <AnimatePresence>
