90e21458385aeef0f7f0e38aff638bf6818f1655
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2025-05-16 12:41:04 +0100

    startProfileCard

--- frontend/src/components/molecules/ProfileCard.js
+++ frontend/src/components/molecules/ProfileCard.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { UserRound } from "lucide-react";
 
 function ProfileCard(props) {
-  return <div>ProfileCard</div>;
+  return (
+    <>
+      <div className="">
+        <div className="w-24 h-24 shadow-md rounded-full bg-main flex items-center justify-center">
+          <UserRound className="text-white" size={42} />
+        </div>
+        <div className=""></div>
+      </div>
+    </>
+  );
 }
 
 export default ProfileCard;
